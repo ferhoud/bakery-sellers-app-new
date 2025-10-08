fee3529815057983f29ca292ef028b5596acfe37
ferhoud <ferhoud@hotmail.com>
2025-10-08 02:46:14 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // En-têtes HTTP pour certains fichiers statiques
   async headers() {
     return [
       {
-        // Service Worker : jamais mis en cache
         source: "/sw.js",
         headers: [
           { key: "Cache-Control", value: "no-store, max-age=0" },
-          // Permet au SW de contrôler tout le scope "/"
           { key: "Service-Worker-Allowed", value: "/" },
         ],
       },
       {
-        // (optionnel) éviter le cache agressif du manifest
         source: "/manifest.json",
         headers: [{ key: "Cache-Control", value: "no-cache" }],
       },
