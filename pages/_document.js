e7f03c77c48fdfcd95f80b4781a2d30044327f79
ferhoud <ferhoud@hotmail.com>
2025-10-10 05:17:25 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,34 +5,8 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        {/* Encodage côté serveur */}
         <meta charSet="utf-8" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-
-        {/* Police texte (Inter) */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* Icônes Material – les deux familles pour couvrir tous les cas */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@200..700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* Réglages d’icônes */}
-        <style>{`
-          .material-symbols-outlined {
-            font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24;
-          }
-          .material-icons { font-family: 'Material Icons'; font-style: normal; }
-        `}</style>
       </Head>
       <body>
         <Main />
