cd6d69bb65e12a894c6c934ea0f09972dbc8054f
ferhoud <ferhoud@hotmail.com>
2025-10-10 07:02:23 +0200

    fix(build): minimal _app/_document/supabaseClient + add /status and /api/ok + disable ESLint during build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 ﻿/** @type {import("next").NextConfig} */
 const nextConfig = {
-  eslint: {
-    // TEMPORAIRE : on ignore les erreurs ESLint pendant le build Vercel
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
 };
-
 module.exports = nextConfig;
--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-// pages/_document.js
-import { Html, Head, Main, NextScript } from "next/document";
-=======
 ﻿import { Html, Head, Main, NextScript } from "next/document";
->>>>>>> deploy-sellers
 
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
