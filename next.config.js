14364c7f3bf1e4a255298ed57b9311a3a9fbc4d7
ferhoud <ferhoud@hotmail.com>
2025-10-10 03:39:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   async redirects() {
     return [
-      {
-        source: '/',
-        destination: '/admin',
-        permanent: false,
-      },
+      { source: '/', destination: '/admin', permanent: false },
     ];
   },
 };
+
+module.exports = nextConfig;
