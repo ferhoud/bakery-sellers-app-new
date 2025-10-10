ee22e801872c8e7edf2a8ee4097a2360ab708d4b
ferhoud <ferhoud@hotmail.com>
2025-10-10 22:24:06 +0200

    Update WeekNav.js

--- components/WeekNav.js
+++ components/WeekNav.js
@@ -1,18 +1,30 @@
-﻿
-import { addDays, startOfWeek, fmtISODate } from "../lib/date";
+// components/weeknav.js
+// Encodage recommandé : UTF-8
 
-export default function WeekNav({ monday, onPrev, onToday, onNext }){
-  const days = Array.from({length:7}).map((_,i)=> addDays(monday,i));
+import { addDays, fmtISODate } from "../lib/date";
+
+const PREV_LABEL  = "Semaine pr\u00E9c\u00E9dente"; // “précédente”
+const TODAY_LABEL = "Semaine en cours";
+const NEXT_LABEL  = "Semaine suivante";
+
+export default function WeekNav({ monday, onPrev, onToday, onNext }) {
+  const days = Array.from({ length: 7 }, (_, i) => addDays(monday, i));
+
   return (
     <div className="flex items-center gap-2 mb-3">
-      const PREV_LABEL = "Semaine pr\u00E9c\u00E9dente"; // “précédente”
-      <button className="btn" onClick={onToday}>Semaine en cours</button>
-      <button className="btn" onClick={onNext}>Semaine suivante</button>
+      <button type="button" className="btn" onClick={onPrev}>
+        {PREV_LABEL}
+      </button>
+      <button type="button" className="btn" onClick={onToday}>
+        {TODAY_LABEL}
+      </button>
+      <button type="button" className="btn" onClick={onNext}>
+        {NEXT_LABEL}
+      </button>
+
       <div className="ml-auto text-sm text-gray-600">
-        {fmtISODate(days[0])} â†’ {fmtISODate(days[6])}
+        {fmtISODate(days[0])} {"\u2192"} {fmtISODate(days[6])}
       </div>
     </div>
   );
 }
-
-
