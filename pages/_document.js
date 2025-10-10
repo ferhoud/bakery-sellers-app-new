74f90bb704b61a27299771d7d72bbeb92026f264
ferhoud <ferhoud@hotmail.com>
2025-10-10 22:17:16 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,10 @@
-﻿// pages/_document.js
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        {/* Important: forcer l'UTF-8 */}
         <meta charSet="utf-8" />
       </Head>
       <body>
