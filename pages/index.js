5ba8ceda92a5264919558ea5495a4286543f820d
ferhoud <ferhoud@hotmail.com>
2025-10-10 03:33:36 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Bienvenue sur le planning des vendeuses 👋</h1>
+      <p>Utilisez /admin pour accéder à la gestion.</p>
+    </div>
+  );
+}
