c49e54bf1465adc62389bc021bfa50f5bb33969f
ferhoud <ferhoud@hotmail.com>
2025-10-10 06:54:14 +0200

    fix: add pages/status.js and remove duplicates

--- pages/status.js
+++ pages/status.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export default function Status() {
-  return <div style={{padding:20,fontFamily:"system-ui"}}>Status OK ✅</div>;
+﻿export default function Status(){
+  return <div style={{padding:20,fontFamily:"system-ui"}}>Status OK ✅ (pages/)</div>;
 }
-=======
-﻿export default function Status(){return <div style={{padding:20,fontFamily:"system-ui"}}>Status OK ✅ (pages/)</div>;}
->>>>>>> deploy-sellers
