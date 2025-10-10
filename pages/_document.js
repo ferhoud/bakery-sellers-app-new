4246621a51c5b0cca22bc4b570167bac8c4cdf8f
ferhoud <ferhoud@hotmail.com>
2025-10-10 04:39:00 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,24 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="fr">
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        {/* Si tu utilises les Material Symbols/Icons, garde ces 2 lignes : */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* Icônes Material (si tu en utilises dans d'autres pages) */}
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@200..700&display=swap"
           rel="stylesheet"
         />
-        {/* Et ce petit style pour les classes "material-symbols-outlined" */}
         <style>{`
           .material-symbols-outlined {
-            font-variation-settings:
-              'FILL' 0,
-              'wght' 400,
-              'GRAD' 0,
-              'opsz' 24;
+            font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24;
           }
         `}</style>
       </Head>
