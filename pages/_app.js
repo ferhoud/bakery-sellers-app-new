f0db2686c0d114194166427a5752b0ae826cefaf
ferhoud <ferhoud@hotmail.com>
2025-10-11 16:19:14 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "@/styles/globals.css"; // si tu as un globals.css, sinon retire cette ligne
+import "@/styles/globals.css"; // supprime cette ligne si tu n'as pas ce fichier
 import { AuthProvider } from "@/lib/useAuth";
 
 export default function MyApp({ Component, pageProps }) {
