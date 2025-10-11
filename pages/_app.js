c4337c0217ecb330f4ba665c67634fdc1748acd5
ferhoud <ferhoud@hotmail.com>
2025-10-11 04:27:38 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js').catch(() => {});
     }
   }, []);
