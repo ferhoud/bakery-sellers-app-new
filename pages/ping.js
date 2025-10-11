ca491a6c172426bafe43c8b7b4c0d37b1652e4e1
ferhoud <ferhoud@hotmail.com>
2025-10-11 07:26:34 +0200

    Create ping.js

---
+++ pages/ping.js
@@ -0,0 +1,8 @@
+export default function Ping() {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>/ping</h1>
+      <p>Cette page fonctionne. Place ce fichier dans <code>pages/ping.js</code>.</p>
+    </div>
+  );
+}
