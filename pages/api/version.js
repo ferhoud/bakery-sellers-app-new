808e3fc2b1252bb2cf716b677a674d58b3b6935c
ferhoud <ferhoud@hotmail.com>
2026-01-04 08:21:22 +0100

    Update version.js

--- pages/api/version.js
+++ pages/api/version.js
@@ -2,7 +2,6 @@
 import { getBuildInfo } from "../../lib/version";
 
 export default function handler(req, res) {
-  // Important: éviter que le navigateur, Vercel ou un service worker te serve une vieille réponse
   res.setHeader("Cache-Control", "no-store, max-age=0");
   res.status(200).json(getBuildInfo());
 }
